e9983b54ed5c3f4a7a9ca4fc4bb5c584247eb70e
ChesterOM <131749381+ChesterOM@users.noreply.github.com>
2024-02-06 18:08:41 +1300

    Add basic CSS styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,18 @@
 import PropTypes from "prop-types"
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
 const Card = (props) => {
     const imageURL = `https://img.pokemondb.net/artwork/large/${props.id}.jpg`;
+    const displayName = capitalizeFirstLetter(props.id)
+    
     return (
         <div className="card" onClick={props.onClick}>
-            <img src={imageURL} alt={props.id} />
-            <span>{props.id}</span>
+            <img src={imageURL} alt={displayName} />
+            <span>{displayName}</span>
         </div>
     )
 }
